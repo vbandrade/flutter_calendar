48f232cd1a61912ac0469626ea07a26988d870bd
@vbandrade <vbandrade@gmail.com>
2018-11-18 08:46:10 -0200

    removed index from calendarevent since is not used

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -56,12 +56,10 @@ class _MyHomePageState extends State<MyHomePage> {
     for (int i = 0; i < diff; i++) {
       DateTime date = start.add(Duration(days: i));
       events.add(CalendarEvent<int>(
-        index: i + 1000,
         date: date,
         event: Random().nextInt(500),
       ));
       events.add(CalendarEvent<int>(
-        index: i,
         date: date,
         event: 1,
       ));
--- lib/src/calendarevent.dart
+++ lib/src/calendarevent.dart
@@ -3,64 +3,6 @@ import 'package:flutter/material.dart';
 /// One day duration to use in the rest of the system.
 const Duration oneDay = const Duration(days: 1);
 
-///
-/// The source for the calendar, this is where to get all the events from.
-///
-/*
-abstract class CalendarSource {
-  SliverScrollViewCalendarElement element;
-
-  List<CalendarEvent> getEvents(DateTime start, DateTime end);
-
-  ///
-  /// The widget for the specific calendar event, this is what to render
-  /// when showing the calendar event.
-  ///
-  Widget buildWidget(BuildContext context, CalendarEvent index);
-
-  ///
-  /// Called on startup to connect the calendar event element to the source.
-  /// This is used to handle the [scroolToDay] call.
-  ///
-  void init(SliverScrollViewCalendarElement key) {
-    element = key;
-    initState();
-  }
-
-  ///
-  /// Scrolls the calendar to the specific datetime set here.  Note
-  /// we only use the month/day/year for this.  The milliseconds is
-  /// *not* correct and not in local time.
-  ///
-  void scrollToDay(DateTime time) {
-    element.scrollToDate(time);
-  }
-
-  ///
-  /// Called when the source is update by a widget redo.
-  ///
-  void didUpdateSource(CalendarSource source) {
-  }
-
-  ///
-  /// Updates the events queue to rebuild the display.
-  ///
-  void updateEvents() {
-    element.updateEvents();
-  }
-
-  ///
-  /// Called to initialize the state for the event.
-  ///
-  void initState();
-
-  ///
-  /// Called when the event is disposed.
-  ///
-  void dispose();
-}
-*/
-
 abstract class CalendarEventElement {
   void updateEvents();
 
@@ -77,14 +19,11 @@ enum CalendarViewType { Schedule, Week, Month }
 ///
 class CalendarEvent<E> {
   CalendarEvent({
-    @required this.index,
     @required this.date,
     this.event,
   });
   DateTime date;
   E event;
-
-  int index;
 
   static const int YEAR_OFFSET = 12 * 31;
   static const int MONTH_OFFSET = 31;
@@ -98,6 +37,6 @@ class CalendarEvent<E> {
 
   @override
   String toString() {
-    return 'CalendarEvent{instant: $date, index: $index}';
+    return 'CalendarEvent{instant: $date, event: $event}';
   }
 }
